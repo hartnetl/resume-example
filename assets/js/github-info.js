000ba9ed457838b061a8298b7870485e00fae4d9
hartnetl <laura.hartnett@outlook.com>
2021-08-31 13:39:31 +0000

    Get api to return error message if user does not exist

--- assets/js/github-info.js
+++ assets/js/github-info.js
@@ -10,18 +10,32 @@ function fetchGitHubInformation(event) {
             <img src="assets/css/loader.gif" alt="loading">
         </div>`
     );
+
+    /*
+    Using promises to retrieve info from github api
+        When X is done, Y will happen.
+        Here it could be
+            When we have gotten a response from the github api
+            Then the function to display it in the gh-user-data div is executed
+            Unless there is an error
+    */
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`) //When the username entered is gotten
+    ).then(  // We want to display it
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        }, function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
+            }
+        });
+
 }
 
-// function fetchGitHubInformation(event) {
 
-//     var username = $("#gh-username").val();
-//     if (!username) {
-//         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
-//         return;
-//     }
-
-//     $("#gh-user-data").html(
-//         `<div id="loader">
-//             <img src="assets/css/loader.gif" alt="loading..." />
-//         </div>`);
-// }
