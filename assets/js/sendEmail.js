bed400500043e931edc45f70adb445f4c00aaaf8
hartnetl <laura.hartnett@outlook.com>
2021-08-31 16:37:11 +0000

    Let contact form send emails

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,34 @@
+function sendMail(contactForm) {
+    emailjs.send("rosie", "Rose", {
+        // These are the 'names' taken from the html form code
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
+
+// function sendMail(contactForm) {
+//     emailjs.send("rosie", "Rose", {
+//         "from_name": contactForm.name.value,
+//         "from_email": contactForm.emailaddress.value,
+//         "project_request": contactForm.projectsummary.value
+//     })
+//     .then(
+//         function(response) {
+//             console.log("SUCCESS", response);
+//         },
+//         function(error) {
+//             console.log("FAILED", error);
+//         }
+//     );
+//     return false;  
+// }
